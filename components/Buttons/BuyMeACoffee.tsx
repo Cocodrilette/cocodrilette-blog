c003a3621338398e1bfb7f8cef00a83542fe667b
cocodrilette <cocodrilette@gmail.com>
2022-11-27 21:13:51 -0500

    error fixed: walled connection checked before send

--- components/Buttons/BuyMeACoffee.tsx
+++ components/Buttons/BuyMeACoffee.tsx
@@ -1,35 +1,74 @@
 import { parseEther } from 'ethers/lib/utils';
-import { usePrepareSendTransaction, useSendTransaction } from 'wagmi';
+import {
+  usePrepareSendTransaction,
+  useSendTransaction,
+  useAccount,
+} from 'wagmi';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ChainIcon } from 'connectkit';
-
-import styles from './SendEth.module.css';
 import { useEffect } from 'react';
 
+import styles from './SendEth.module.css';
+
 export default function BuyMeACoffee() {
+  const { isConnected } = useAccount();
   const { config } = usePrepareSendTransaction({
     request: {
       to: '0x6cf6c2ac234eb8bec53e28ad28b16421fabe4f37',
       value: parseEther('0.00090'),
     },
   });
-  const { sendTransaction, isLoading, isError, isSuccess } =
+  const { sendTransactionAsync, isLoading, isError } =
     useSendTransaction(config);
-
   const handleOnClick = async () => {
-    sendTransaction();
+    if (!isConnected) {
+      toast.error('Please connect your wallet first.', {
+        toastId: 'transactionFailed',
+        position: 'top-center',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+      return;
+    }
+
+    try {
+      const res = await sendTransactionAsync();
+      if (res) {
+        toast.success('Transaction sent', {
+          toastId: 'transactionSent',
+          position: 'top-center',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'dark',
+        });
+      }
+    } catch (error) {
+      return;
+    }
   };
 
   const button = () => {
     const buttonDefaultStyle =
-      'bg-black w-52 h-12 text-white rounded-md py-2 px-3';
-    const buttonErrorStyle = 'bg-red-500 h-12 text-white rounded-md py-2 px-3';
+      'bg-black h-12 text-white rounded-md py-2 px-3 shadow-sm';
+    const buttonLoadingtStyle =
+      'bg-black w-52 h-12 text-white rounded-md py-2 px-3 shadow-sm';
+    const buttonErrorStyle =
+      'bg-red-500 h-12 text-white rounded-md py-2 px-3 shadow-sm';
 
     if (isLoading) {
       return (
-        <button className={buttonDefaultStyle}>
+        <button className={buttonLoadingtStyle}>
           <AiOutlineLoading3Quarters className="animate-spin m-auto" />
         </button>
       );
@@ -51,9 +90,9 @@ export default function BuyMeACoffee() {
   };
 
   useEffect(() => {
-    if (isSuccess) {
-      toast.success('Transaction sent', {
-        toastId: 'transactionSent',
+    if (isError) {
+      toast.error('Transaction failed', {
+        toastId: 'transactionFailed',
         position: 'top-center',
         autoClose: 3000,
         hideProgressBar: false,
@@ -64,7 +103,7 @@ export default function BuyMeACoffee() {
         theme: 'dark',
       });
     }
-  }, [isSuccess]);
+  }, [isError]);
 
   return (
     <>
